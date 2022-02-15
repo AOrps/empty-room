9b7c3bed9f513af4d51958fd51587aeeb254b212
AOrps <aeo34@njit.edu>
2022-02-15 11:16:18 -0500

    Backend DB + analyze.py setup locally, util a bit fleshed out and backend makefile

---
+++ data-processing/analyze.py
@@ -0,0 +1,15 @@
+"""
+Takes a hidden data-collection dir and analyzes it for stuff in DB schema.
+
+PIP Requirements: pandas
+"""
+
+# import pandas
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()

41be996318ae9d3d909ed9d9ba44404532f1894a
AOrps <aeo34@njit.edu>
2022-02-16 21:37:33 -0500

    data analysis script and stuff

--- data-processing/analyze.py
+++ data-processing/analyze.py
@@ -4,12 +4,27 @@ Takes a hidden data-collection dir and analyzes it for stuff in DB schema.
 PIP Requirements: pandas
 """
 
-# import pandas
+import pandas as pd
+import os
 
 
 def main():
-    pass
+    baseDir = "data-collection"
+    # df = pd.read_csv(f"{baseDir}/ACCT.csv")
+    # pd.read_csv(f"{baseDir}/AD.csv")
+    # print([f"{baseDir}/{f}" for f in os.listdir("data-collection")])
+    df = pd.concat([pd.read_csv(f"{baseDir}/{f}") for f in os.listdir("data-collection")])
 
+    for _, row in df.iterrows():
+        if row['Location'] == " " or row['Location'] == "":
+            row['Location'] = "nan" 
+
+        if row['Instructor'] != "nan":
+            instructor = "".join(row["Instructor"].split(","))
+            row['Instructor'] = instructor
+        print(f"{row['Course']},{row['Title']},{row['Days']},{row['Times']},{row['Location']},{row['Instructor']}")
+
+    # print(df.to_string())
 
 if __name__ == "__main__":
     main()
